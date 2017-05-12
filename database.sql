1321e51db37c7f9a61a4d82f35fd1625927d4869
Kebab95 <kebalazs95@gmail.com>
2017-05-12 23:19:02 +0200

    Update #5 Adatbázis fájl optimalizálása, tesztek során hibásan importálta be az adatbázis és tapasztalatok alapján javítva

--- database.sql
+++ database.sql
@@ -4,56 +4,11 @@
 -- ------------------------------------------------------
 -- Server version	5.7.18-0ubuntu0.16.04.1
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Current Database: `webtown`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `webtown` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `webtown`;
 
 USE `webtown`;
 
---
--- Table structure for table `rendelesek`
---
 
-DROP TABLE IF EXISTS `rendelesek`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `rendelesek` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` char(255) NOT NULL,
-  `email` char(255) NOT NULL,
-  `kiszalitasi_cim` char(255) NOT NULL,
-  `termekID` int(11) NOT NULL,
-  `darab` int(11) NOT NULL,
-  `ar` int(11) NOT NULL,
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `rendelesek_termekek_id_fk` (`termekID`),
-  CONSTRAINT `rendelesek_termekek_id_fk` FOREIGN KEY (`termekID`) REFERENCES `termekek` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `rendelesek`
---
-
-LOCK TABLES `rendelesek` WRITE;
-/*!40000 ALTER TABLE `rendelesek` DISABLE KEYS */;
-
-/*!40000 ALTER TABLE `rendelesek` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `termekek`
@@ -78,19 +33,31 @@ CREATE TABLE `termekek` (
 -- Dumping data for table `termekek`
 --
 
-LOCK TABLES `termekek` WRITE;
-/*!40000 ALTER TABLE `termekek` DISABLE KEYS */;
 INSERT INTO `termekek` VALUES (1,'Téliszalámi',2000,0,'2017-05-08 14:04:43','2017-05-08 14:04:43',0),(2,'Gumikacsa',3000,0,'2017-05-08 14:04:43','2017-05-08 14:04:43',0),(3,'Uborka',2800,1,'2017-05-08 14:04:43','2017-05-08 14:04:43',0),(4,'Gesztenye',1000,1,'2017-05-08 14:04:43','2017-05-08 14:04:43',0);
-/*!40000 ALTER TABLE `termekek` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+
+--
+-- Table structure for table `rendelesek`
+--
+
+DROP TABLE IF EXISTS `rendelesek`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rendelesek` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` char(255) NOT NULL,
+  `email` char(255) NOT NULL,
+  `kiszalitasi_cim` char(255) NOT NULL,
+  `termekID` int(11) NOT NULL,
+  `darab` int(11) NOT NULL,
+  `ar` int(11) NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `rendelesek_termekek_id_fk` (`termekID`),
+  CONSTRAINT `rendelesek_termekek_id_fk` FOREIGN KEY (`termekID`) REFERENCES `termekek` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 -- Dump completed on 2017-05-12 15:11:06
